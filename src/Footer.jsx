95110287fbf1ebde79cc723ca023a2c11cbb4071
Aaron Sala <salaaron2@gmail.com>
2024-03-20 12:07:43 +0300

    vite react project

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    
+      <footer>
+          <p>&copy; {new Date().getFullYear() } The Arfrim Technologies</p>
+      </footer>
+    
+  );
+}
+export default Footer;
